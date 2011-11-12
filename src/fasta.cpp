d85b426a2301e708a9ff7743421362b20e1aa350
dhruvbird <dhruvbird@1860e753-9f50-4fd8-90da-c2bea331e5cf>
2011-11-12 22:54:41 +0000

    Added a fasta file reader
    git-svn-id: svn://dhruvbird.com/cse549/project@40 1860e753-9f50-4fd8-90da-c2bea331e5cf

---
+++ src/fasta.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <assert.h>
+
+using namespace std;
+
+class FastaReader {
+    FILE *pf;
+    std::string fpath;
+    char *buff;
+    int bsz;
+    size_t sz = 0;
+
+    void
+    readsome() {
+        // Assume that buff is empty
+        ssize_t ret = ::getline(&this->buff, &this->sz, this->pf);
+        if (ret < 0) {
+            this->bsz = 0;
+        }
+        if (ret > 0 && this->buff[ret-1] == '\n') {
+            --ret;
+        }
+        bsz = ret;
+    }
+
+public:
+    FastaReader(std::string path)
+        : pf(NULL), fpath(path), buff(NULL), bsz(0) {
+        this->pf = fopen(path.c_str(), "r");
+        assert(this->pf);
+        ::getline(&this->buff, &this->sz, this->pf);
+    }
+
+    int
+    read(char *out, int size) {
+        int ctr = 0;
+        int tr = size;
+        while (tr) {
+            if (!this->bsz) {
+                readsome();
+                if (!this->bsz) {
+                    return ctr;
+                }
+            }
+
+            int m = this->bsz < size ? this->bsz : size;
+            memmove(out, this->buff, m);
+
+            int rem = this->bsz - m;
+            if (rem) {
+                memmove(this->buff, this->buff + m, rem);
+            }
+
+            this->bsz = rem;
+            tr -= m;
+            ctr += m;
+        }
+        return ctr;
+    }
+
+
+};
