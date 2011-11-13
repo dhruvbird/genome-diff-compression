5c1193abbd0e3bc371a779a6b7e5aac5f224c69b
dhruvbird <dhruvbird@1860e753-9f50-4fd8-90da-c2bea331e5cf>
2011-11-13 17:23:48 +0000

    Major bug-fix in Suffix Array Generation & Added code to compute pairwise Longest Common Prefix Length
    git-svn-id: svn://dhruvbird.com/cse549/project@42 1860e753-9f50-4fd8-90da-c2bea331e5cf

--- src/suffix_array.cpp
+++ src/suffix_array.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <assert.h>
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,6 +13,7 @@ typedef pair<int, int> pii_t;
 typedef vector<char> vc_t;
 typedef vector<int>  vi_t;
 typedef vector<vi_t> vvi_t;
+typedef vector<pii_t> vpii_t;
 
 int log2(int n) {
     int lg2 = 0;
@@ -44,17 +47,19 @@ get_rank(vi_t &cmp, int i) {
 
 struct SizeDoubler {
     vi_t &cmp;
+    int sz;
 
-    SizeDoubler(vi_t &c)
-        : cmp(c)
+
+    SizeDoubler(vi_t &c, int _sz)
+        : cmp(c), sz(_sz)
     { }
 
     bool
     operator() (int lhs, int rhs) const {
         int l = this->cmp[lhs];
         int r = this->cmp[rhs];
-        int l1 = get_rank(cmp, lhs+1);
-        int r1 = get_rank(cmp, rhs+1);
+        int l1 = get_rank(cmp, lhs + this->sz);
+        int r1 = get_rank(cmp, rhs + this->sz);
 
         if (l == r) {
             return l1 < r1;
@@ -66,9 +71,10 @@ struct SizeDoubler {
 };
 
 void
-print_ranks(vvi_t &dp) {
+print_ranks(std::string const &s, vvi_t &dp) {
     int n = dp.empty() ? 0 : dp[0].size();
     for (size_t r = 0; r < n; ++r) {
+        printf("%3c", s[r]);
         for (size_t c = 0; c < dp.size(); ++c) {
             printf("%4d", dp[c][r]);
         }
@@ -78,6 +84,7 @@ print_ranks(vvi_t &dp) {
 
 struct SuffixArray {
     vvi_t dp;
+    size_t ssize;
 
     void
     fill_indexes(vi_t &indexes, size_t n) {
@@ -92,9 +99,11 @@ struct SuffixArray {
     make_ranks1(vi_t &data, vi_t &indexes, vi_t &ranks) {
         ranks.clear();
         const size_t n = indexes.size();
+        assert(n > 0);
+
         ranks.resize(n);
         int rank = 1;
-        int prev = n > 0 ? data[indexes[0]] : 0;
+        int prev = data[indexes[0]];
         for (size_t i = 0; i < n; ++i) {
             cerr<<"data: "<<data[indexes[i]]<<", prev: "<<prev<<endl;
             if (data[indexes[i]] != prev) {
@@ -106,16 +115,18 @@ struct SuffixArray {
     }
 
     void
-    make_ranks2(vi_t &data, vi_t &indexes, vi_t &ranks) {
+    make_ranks2(vi_t &data, vi_t &indexes, vi_t &ranks, int sz) {
         ranks.clear();
         const size_t n = indexes.size();
+        assert(n > 0);
+
         ranks.resize(n);
         int rank = 1;
-        int x = n > 0 ? data[indexes[0]] : 0;
-        int y = n > 0 ? get_rank(data, indexes[0]+1) : 0;
+        int x = data[indexes[0]];
+        int y = get_rank(data, indexes[0]+sz);
         for (size_t i = 0; i < n; ++i) {
             int r1 = data[indexes[i]];
-            int r2 = get_rank(data, indexes[i]+1);
+            int r2 = get_rank(data, indexes[i]+sz);
             printf("indexes[%d]: %d\n", i, indexes[i]);
             printf("data: (%d, %d), prev: (%d, %d)\n", r1, r2, x, y);
             if (x != r1 || y != r2) {
@@ -126,13 +137,24 @@ struct SuffixArray {
         }
     }
 
+    void
+    sorted_indexes(vi_t &pos) {
+        assert(this->dp.size() > 0);
+        vi_t &_dp = this->dp.back();
+        pos.clear();
+        pos.resize(this->ssize);
+        for (size_t i = 0; i < pos.size(); ++i) {
+            pos[_dp[i]-1] = i;
+        }
+    }
 
 public:
     SuffixArray(std::string const &s) {
-        int n = s.size();
-        if (n == 0) {
+        this->ssize = s.size();
+        if (this->ssize == 0) {
             return;
         }
+        const int n = this->ssize;
         dp.resize(log2(n) + ((n & (n-1)) == n ? 1 : 2));
         vi_t buf(s.begin(), s.end());
         vi_t indexes;
@@ -148,26 +170,92 @@ public:
             cerr<<"i: "<<i<<endl;
             this->fill_indexes(indexes, n);
             cerr<<"Done filling indexes\n";
-            std::sort(indexes.begin(), indexes.end(), SizeDoubler(this->dp[i-1]));
+            std::sort(indexes.begin(), indexes.end(), SizeDoubler(this->dp[i-1], 1 << (i-1)));
             cerr<<"Done sorting\n";
-            this->make_ranks2(this->dp[i-1], indexes, this->dp[i]);
+            this->make_ranks2(this->dp[i-1], indexes, this->dp[i], 1 << (i-1));
             cerr<<"Done making ranks\n";
             // return;
         }
     }
+
+    void
+    lcp_pairwise(vi_t &lcp) {
+        assert(this->dp.size() > 0);
+        vi_t &_dp = this->dp.back();
+
+        lcp.clear();
+        lcp.resize(this->ssize);
+
+        vi_t pos;
+        this->sorted_indexes(pos);
+
+        if (this->ssize < 1) {
+            return;
+        }
+
+        const int n = this->ssize;
+        for (size_t i = 0; i < this->ssize - 1; ++i) {
+            int _lcp = 0, x = pos[i], y = pos[i+1];
+            for (int j = this->dp.size() - 1; j >= 0 && x < n && y < n; --j) {
+                // printf("dp[%d][%d] == %d, dp[%d][%d] == %d\n", j, x, dp[j][x], j, y, dp[j][y]);
+                if (dp[j][x] == dp[j][y]) {
+                    _lcp += (1 << j);
+                    x += (1 << j);
+                    y += (1 << j);
+                }
+            }
+            lcp[i] = _lcp;
+            printf("LCP [%d, %d] == %d\n", pos[i], pos[i+1], _lcp);
+        }
+    }
+
+    // Returns the lengths of longest common prefix of every string in
+    // set-2 with some string in set-1.
+    // 
+    // The vector 'lcp' is populated with pair of type (index, length)
+    // where the i'th entry's 'index' is the index in the original
+    // string where the string starting at index 'i' matches with
+    // 'length' characters of match.
+    void
+    lcp_across_sets(int set2_index, vpii_t &lcp) {
+    }
+
 };
+
+void
+print_suffix_array(std::string const &s, SuffixArray &sa) {
+    vi_t pos;
+    sa.sorted_indexes(pos);
+    for (size_t i = 0; i < pos.size(); ++i) {
+        printf("%3d: %s\n", pos[i], s.substr(pos[i]).c_str());
+    }
+}
+
+
 
 namespace suffix_array {
     int
     test() {
-        SuffixArray sa1("banana");
-        print_ranks(sa1.dp);
 
         SuffixArray sa2("yabadabadoo");
-        print_ranks(sa2.dp);
+        print_ranks("yabadabadoo", sa2.dp);
+        print_suffix_array("yabadabadoo", sa2);
+
+        vi_t lcp;
+        sa2.lcp_pairwise(lcp);
+
+        cout<<"log_2(11): "<<log2(11)<<endl;
+        return 0;
+
+        SuffixArray sa1("banana");
+        print_ranks("banana", sa1.dp);
 
         SuffixArray sa3("mississippi");
-        print_ranks(sa3.dp);
+        print_ranks("mississippi", sa3.dp);
+
+        std::string s4 = "Regular expressions, or just regexes, are at the core of Perl's text processing, and certainly are one of the features that made Perl so popular. All Perl programmers pass through a stage where they try to program everything as regexes, and when that's not challenging enough, everything as a single regex. Perl's regexes have many more features than I can, or want, to present here, so I include those advanced features I find most useful and expect other Perl programmers to know about without referring to perlre, the documentation page for regexes.";
+        SuffixArray sa4(s4);
+        print_ranks(s4, sa4.dp);
     }
 }
 
